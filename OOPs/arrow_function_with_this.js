3f73372f1fb3858cbdabc0cd629e44a11339ea34
Adi <adinarayana4444@gmail.com>
2022-08-06 13:55:51 +0530

    FE Dsa

---
+++ OOPs/arrow_function_with_this.js
@@ -0,0 +1,16 @@
+var teacher = "Adi";
+let askQuestion = (question) => {
+    console.log(this.teacher, question);
+}
+
+let obj = {
+    teacher: 'Ram',
+    ask: askQuestion
+}
+
+obj.ask("what is closure ?");
+
+let obj1 = {
+    ask: askQuestion
+}
+obj1.ask("what is JS?")
