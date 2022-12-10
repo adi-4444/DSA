4c80b4e05dee9a5940ea9387e836b1772b526e5b
Adi Narayana Reddy <adinarayana4444@gmail.com>
2022-12-10 22:41:58 +0530

    FE DSA

--- Finding Armstrong.js
+++ Finding Armstrong.js
@@ -1,12 +1,12 @@
 // finding the given number is Armstrong ot not
 
 function armStrong(input) {
-   let result=0;
+   let result = 0;
    let num = input;
-   while (num>0){
+   while (num > 0) {
       rem = num % 10
-      result += rem * rem* rem
-      num = parseInt(num /10)
+      result += rem * rem * rem
+      num = parseInt(num / 10)
    }
    if (result === input) {
       console.log(" Given number is ArmStrong")
--- counter.js
+++ counter.js
@@ -1,12 +1,12 @@
-function counter () {
-   let a = 0;
-   function increment() {
-     return a = a + 1;
-   }
-   return a;
+function counter() {
+  let a = 0;
+  function increment() {
+    return a = a + 1;
+  }
+  return a;
 }
 
-let incrFunc = counter ();
+let incrFunc = counter();
 console.log(incrFunc());
 console.log(incrFunc());
 console.log(counter()());
